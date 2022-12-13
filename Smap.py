7635e9cf0bc91fc40785e9351cf3b82f3277a5e4
Tansej <tansejcz@gmail.com>
2022-12-13 14:56:17 +0100

    Dataset column names and basic tools for python

---
+++ Smap.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import svm
+
+#Path to dataset
+pathDataset = 'C:\\Users\\Tansej\\Desktop\\SMAPProjekt\\Projekt\\dataset\\mails.csv'
